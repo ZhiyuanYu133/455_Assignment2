b6459d72d68efcec48d1c7dbda4f8f84772787f8
yuihan <juyi@ualberta.ca>
2022-10-09 20:03:14 -0600

    new

--- gtp_connection.py
+++ gtp_connection.py
@@ -13,6 +13,7 @@ import numpy as np
 import re
 from sys import stdin, stdout, stderr
 from typing import Any, Callable, Dict, List, Tuple
+import time
 
 from board_base import (
     is_black_white,
@@ -304,9 +305,11 @@ class GtpConnection:
     ==========================================================================
     """
 
-    def gogui_timer(self,arg):
+    def timelimit(self,seconds:int):
+        if (1 <= seconds <= 100):
+            max_time = seconds
         # need to implement
-        return self
+        return max_time
 
     def gogui_rules_final_result_cmd(self, args):
         """ Implement this method correctly """
@@ -358,48 +361,29 @@ class GtpConnection:
         # change this method to use your solver
         board_color = args[0].lower()
         color = color_to_int(board_color)
-        gtp_moves: List[str] = self.__get_legal_moves()
-
-        # if game over (no other moves) we resign and return a pass
-        if len(gtp_moves) == 0:
-            self.respond('resign')
-            return
-
         move = self.go_engine.get_move(self.board, color)
         if move is None:
             self.respond('unknown')
             return
-
-        # poor implementation (just to get rough idea)
-        # get the winner (and move) from solver and check if the current player is the winner
-        # If current player is winner, then we check if move is legal and if so, we will play it
-        # If current player is not the winner, or the timer for solver takes too long, then we play a random move 
-        winner = self.solve_cmd(self.board, color)
-
-        if (time <= timelimit) and winner == True:
-            move = winner.move  # get winning move given by solve_cmd
-            if self.board.is_legal(move, color):
-                self.board.play_move(move, color)
-                self.respond(move_as_string)
-            else:
-                self.respond("Illegal move: {}".format(move_as_string))
-
+            
+        move_coord = point_to_coord(move, self.board.size)
+        move_as_string = format_point(move_coord)
+        if self.board.is_legal(move, color):
+            self.board.play_move(move, color)
+            self.respond(move_as_string)
         else:
-            move_coord = point_to_coord(move, self.board.size)
-            move_as_string = format_point(move_coord)
-
-            if self.board.is_legal(move, color):
-                self.board.play_move(move, color)
-                self.respond(move_as_string)
-            else:
-                self.respond("Illegal move: {}".format(move_as_string))  
+            self.respond("Illegal move: {}".format(move_as_string))
+            
             
     def solve_cmd(self, args: List[str]) -> None:
         # remove this respond and implement this method
-        toPlay = self.board.current_player
+        limit = self.timelimit()
+        winner = None; 
+        #while not (self.)
 
-        return winner, move
 
+
+        self.respond('Implement This for Assignment 2')
 
     """
     ==========================================================================
